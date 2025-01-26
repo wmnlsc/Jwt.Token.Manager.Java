9a5ed20addbbf16a8ae1d56d9d97526298b647a3
Jose Beltre <jbeltre158@gmail.com>
2025-01-26 15:21:29 -0500

    Library deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,21 +9,18 @@ group = "com.bbhub"
 version = "1.0.0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+    withJavadocJar()
 }
 
 repositories {
-    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -32,14 +29,33 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-        }
-    }
-    repositories {
-        maven {
-            url = uri("$buildDir/repo")
+
+            pom {
+                name.set(project.name)
+                description.set("A library for managing JSON Web Tokens (JWTs)")
+                url.set("https://github.com/wmnlsc/Jwt.Token.Manager.Java")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("wmnlsc")
+                        name.set("Jose Beltre")
+                        email.set("jbeltre158@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/wmnlsc/Jwt.Token.Manager.Java.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:wmnlsc/Jwt.Token.Manager.Java.git")
+                    url.set("https://github.com/wmnlsc/Jwt.Token.Manager.Java")
+                }
+            }
         }
     }
 }
